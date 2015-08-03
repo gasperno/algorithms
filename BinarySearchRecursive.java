916668c6349ae69be177b23d007722911ec432b8
gasperno <harsha049@gmail.com>
2015-08-03 22:02:42 +0000

    BinarySearch old version failed for border cases

--- BinarySearchRecursive.java
+++ BinarySearchRecursive.java
@@ -6,31 +6,33 @@ public class BinarySearchRecursive{
         //recursive binary search
         int[] input={1,2,3,4,5,6,7,8,9,10,11};
         
-        int val=6;
+        int val=2;
         System.out.println("Start");
         for(int i=0;i<input.length;i++){
         val=input[i];
-        int index=binarySearchRecursive(input,0,input.length,val);
+        int index=binarySearchRecursive(input,0,input.length-1,val);
         if(index!=-1){
             System.out.println("Found at "+index);
         }else{
-            System.out.println("Not found");
+            System.err.println("Not found");
         }
+        //break;
         }
     }
     private static int binarySearchRecursive(int[] input,int start, int end, int value){
         int mid=start+(end-start)/2;
         int ret=-1;
         if(start>end){return ret;}
+        //System.out.println(start+" "+end+" "+value+" "+mid);
         if(input[mid]==value){
             if(debug) System.out.println("Found at"+mid+"["+input[mid]+"]");
             return mid;
         }if(input[mid]>value){
             if(debug) System.out.println("Recursing left>>"+start+","+mid+"["+input[mid]+"]");
-            ret=binarySearchRecursive(input,start,mid,value);
+            ret=binarySearchRecursive(input,start,mid-1,value);
         }else{
             if(debug) System.out.println("Recursing right<<"+mid+","+end+"["+input[mid]+"]");
-            ret=binarySearchRecursive(input,mid,end,value);
+            ret=binarySearchRecursive(input,mid+1,end,value);
         }
         return ret;
     }
